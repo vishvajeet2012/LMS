d24899f02f81a96c2988e6b1656b3884fae515d0
vishvajeet shukla <djritikshukla@gmail.com>
2024-12-12 12:54:14 +0530

    as you want

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+<h1 className='text-emerald-500 font-mono font-extrabold text-2xl text-center'>this is vishu</h1>
+    </>
+  )
+}
+
+export default App
